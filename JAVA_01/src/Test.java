0f261d42e31a1a326015c67bcb96e2010c0fb858
Meenal93 <51297351+Meenal93@users.noreply.github.com>
2019-06-02 23:36:22 +0530

    Add files via upload

---
+++ JAVA_01/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test
+
+{//Start of the class body
+	
+    public static void main(String[] args) {
+    	
+    	Test t = new Test();
+    	
+    	System.out.println(t);
+    	
+  
+		
+	}
+	
+
+}//End of the class body
